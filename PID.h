4d0e2d010be6ec0a9742f7e94b6fa90295aa0bef
Renan Moreira <renanmoreira17@gmail.com>
2020-05-12 19:40:59 -0300

    Commit inicial
    adicionados arquivos .c e .h

---
+++ PID.h
@@ -0,0 +1,63 @@
+/*
+ * PID.h
+ *
+ *  Created on: Apr 14, 2020
+ *      Author: renanmoreira
+ */
+
+#ifndef PID_H_
+#define PID_H_
+
+#include "stdint.h"
+
+typedef struct {
+	double Kp;
+	double Ti;
+	double Td;
+	double C0;
+	double C1;
+	double C2;
+	double Setpoint;
+	double sample_period;
+	double error_state[2];
+	double input_state[2];
+	double output;
+	double max_output;
+	double min_output;
+} PID_t;
+
+void PID_init(PID_t* pid, uint8_t reset, double Kp, double Ti, double Td, double max_output, double min_output, double sample_period);
+void PID_set_setpoint(PID_t* pid, double Setpoint);
+void PID_set_parameters(PID_t* pid, double Kp, double Ti, double Td);
+void PID_set_limits(PID_t* pid, double max_output, double min_output);
+void PID_set_sample_period(PID_t* pid, double sample_period);
+
+__attribute__((always_inline)) static inline
+double PID_compute(PID_t* pid, double input)
+{
+	double error = pid->Setpoint-input;
+	/* y[n] = y[n-1] + A0 * x[n] + A1 * x[n-1] + A2 * x[n-2]  */
+	pid->output += (pid->C1 * error) +
+				   (pid->C0 * pid->error_state[0]) +
+				   (pid->C2 * (2.0*pid->input_state[0] - input - pid->input_state[1]));
+
+	//previnir windup
+	if (pid->output > pid->max_output) pid->output = pid->max_output;
+	if (pid->output < pid->min_output) pid->output = pid->min_output;
+
+	/* Update state */
+	pid->error_state[1] = pid->error_state[0];
+	pid->error_state[0] = error;
+	pid->input_state[1] = pid->input_state[0];
+	pid->input_state[0] = input;
+
+	/* return to application */
+	return (pid->output);
+
+}
+
+
+
+
+
+#endif /* PID_H_ */
